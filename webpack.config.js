e438405f45d314ced79d277fbeecb0a748436f54
Matt Mapplebeck <mmapplebeck@about.com>
2017-04-18 17:45:38 -0400

    remove publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
     filename: 'bundle.js'
   },
   devtool: 'source-map',
